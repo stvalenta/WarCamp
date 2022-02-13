7e1f2b76195e179f75599807b7a1768f251043d2
Stanislav Valenta <91445762+stvalenta@users.noreply.github.com>
2022-02-13 19:02:47 +0100

    drobné zmeny

--- lekce3/index.js
+++ lekce3/index.js
@@ -1,16 +1,19 @@
 fetch("https://data.cesko.digital/obce/1/obce.json")
     .then(response => response.json())
     .then(data => {
+      document.querySelector("#zobraz").innerHTML = "<thead><tr><th>Město</th><th>ID schránky</th></tr></thead><tbody>"
       for(let x=0; x<data.municipalities.length; x++){
       let nedata = data.municipalities[x].adresaUradu.kraj;
       if (nedata == "Jihočeský"){
         let mesto = data.municipalities[x].adresaUradu.obec;
         let schranka = data.municipalities[x].datovaSchrankaID;
 
-        document.querySelector("#zobraz").innerHTML += "<li>Město: " + mesto + "  Datová schránka: " + schranka + "</li>";
+        document.querySelector("#zobraz").innerHTML += "<tr><td>" + mesto + "</td><td>" + schranka + "</td></tr>";
+
 
       }
     }
-      
+    document.querySelector("#zobraz").innerHTML += "</tbody></table></div>"
+    document.getElementById("prodleva").style.display = "none";  
     })
     
