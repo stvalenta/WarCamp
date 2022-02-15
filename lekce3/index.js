f906a7fec7c469bed22fc6fdc199ba29bbd923f5
Stanislav Valenta <91445762+stvalenta@users.noreply.github.com>
2022-02-16 00:49:30 +0100

    strankovani

--- lekce3/index.js
+++ lekce3/index.js
@@ -1,16 +1,43 @@
+
 fetch("https://data.cesko.digital/obce/1/obce.json")
   .then(response => response.json())
-  .then(data => {
-    document.querySelector("#zobraz").innerHTML = "<thead><tr><th>Město</th><th>ID schránky</th></tr></thead><tbody>"
+  .then(data => {$(document).ready(function(){
+    $('#zobraz').after('<div id="nav"></div>');
+    var rowsShown = 10;
+    var rowsTotal = $('#zobraz tbody tr').length;
+    var numPages = rowsTotal/rowsShown;
+    for(i = 0;i < numPages;i++) {
+        var pageNum = i + 1;
+        $('#nav').append('<a href="#" rel="'+i+'">'+pageNum+'</a> ');
+    }
+    $('#zobraz tbody tr').hide();
+    $('#zobraz tbody tr').slice(0, rowsShown).show();
+    $('#nav a:first').addClass('active');
+    $('#nav a').bind('click', function(){
+
+        $('#nav a').removeClass('active');
+        $(this).addClass('active');
+        var currPage = $(this).attr('rel');
+        var startItem = currPage * rowsShown;
+        var endItem = startItem + rowsShown;
+        $('#zobraz tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+                css('display','table-row').animate({opacity:1}, 300);
+    });
+});
+    let output = "<thead><tr><th>Město</th><th>ID schránky</th></tr></thead><tbody>";
     for(let x=0; x<data.municipalities.length; x++){
       let nedata = data.municipalities[x].adresaUradu.kraj;
       if (nedata == "Jihočeský"){
         let mesto = data.municipalities[x].adresaUradu.obec;
         let schranka = data.municipalities[x].datovaSchrankaID;
-        document.querySelector("#zobraz").innerHTML += "<tr><td>" + mesto + "</td><td>" + schranka + "</td></tr>";
+        output += "<tr><td>" + mesto + "</td><td>" + schranka + "</td></tr>";
       }
     }
-  document.querySelector("#zobraz").innerHTML += "</tbody></table></div>"
+  output += "</tbody></table></div>";
+  document.querySelector("#zobraz").innerHTML += output;
   document.getElementById("prodleva").style.display = "none";  
   })
+
+
+
     
--- lekce3/level/index.js
+++ lekce3/level/index.js
@@ -1,26 +1,37 @@
+const vulebozi = document.querySelector('#kraje');
+vulebozi.addEventListener('click', function(){
+  document.getElementById('cekani').style.display = "block";
+});
 fetch("https://data.cesko.digital/obce/1/obce.json")
   .then(response => response.json())
   .then(data => {
     const pokus = [];
+
+    output = "<thead><tr><th>Město</th><th>ID schránky</th></tr></thead><tbody>";
     for(let x=0; x<data.municipalities.length; x++){
       var nedata = data.municipalities[x].adresaUradu.kraj;
       pokus[x] = nedata; 
     }
     const unique = pokus.filter((kraje, index) => pokus.indexOf(kraje) == index);
-    for (let i=0; i<unique.length -1; i++){
+    for (let i=-1; i<unique.length -1; i++){
       document.getElementById('kraje').innerHTML += "<option value='" + unique[i] + "'>" + unique[i] + "</option>";
       const selectElement = document.querySelector('#kraje');
-      selectElement.addEventListener('change', (event) => {
-        document.querySelector("#zobraz").innerHTML = "<thead><tr><th>Město</th><th>ID schránky</th></tr></thead><tbody>"
-          for(let y=0; y<data.municipalities.length; y++){
-            var adata = data.municipalities[y].adresaUradu.kraj;
-            if (adata == event.target.value){
-                let mesto = data.municipalities[y].adresaUradu.obec;
-                let schranka = data.municipalities[y].datovaSchrankaID;
-                document.querySelector("#zobraz").innerHTML += "<tr><td>" + mesto + "</td><td>" + schranka + "</td></tr>";
-            }
+      selectElement.addEventListener('change', (event) => {       
+        for(let y=0; y<data.municipalities.length; y++){
+          var adata = data.municipalities[y].adresaUradu.kraj;
+          if (adata == event.target.value){
+              let mesto = data.municipalities[y].adresaUradu.obec;
+              let schranka = data.municipalities[y].datovaSchrankaID;
+              output += "<tr><td>" + mesto + "</td><td>" + schranka + "</td></tr>";
           }
+          
+        }
+
+        output += "</tbody></table>";
+        document.querySelector("#zobraz").innerHTML = output;
+        document.getElementById('cekani').style.display = "none";
       });
     }
-  document.querySelector("#zobraz").innerHTML += "</tbody></table></div>"
-  })
+    
+  
+  });    
