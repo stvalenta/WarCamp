81c8f12186846fa1676c875bacb812637aecd3b2
Stanislav Valenta <91445762+stvalenta@users.noreply.github.com>
2022-02-17 13:32:11 +0100

    už jen čekací ikona

--- lekce3/level/index.js
+++ lekce3/level/index.js
@@ -1,6 +1,8 @@
 fetch("https://data.cesko.digital/obce/1/obce.json")
   .then(response => response.json())
   .then(data => {
+    $('#zobraz').after('<div id="nav"></div>');
+    
     const pokus = [];
     
     for(let x=0; x<data.municipalities.length; x++){
@@ -11,8 +13,8 @@ fetch("https://data.cesko.digital/obce/1/obce.json")
     for (let i=-1; i<unique.length -1; i++){
       document.getElementById('kraje').innerHTML += "<option value='" + unique[i] + "'>" + unique[i] + "</option>";
       const selectElement = document.querySelector('#kraje');
-      
       selectElement.addEventListener('change', (event) => {
+        document.querySelector("#nav").innerHTML = "";
         output = "<thead><tr><th>Město</th><th>ID schránky</th></tr></thead><tbody>";
         document.getElementById('cekani').style.display = "block";
         
@@ -22,21 +24,34 @@ fetch("https://data.cesko.digital/obce/1/obce.json")
               let mesto = data.municipalities[y].adresaUradu.obec;
               let schranka = data.municipalities[y].datovaSchrankaID;
               output += "<tr><td>" + mesto + "</td><td>" + schranka + "</td></tr>";
-        
-
-        
-          }
+          }  
         }
         output += "</tbody></table>";
         document.querySelector("#zobraz").innerHTML = output;
-        
+        var rowsShown = 10;
+        var rowsTotal = $('#zobraz tbody tr').length;
+        var numPages = rowsTotal/rowsShown;
+        for(i = 0;i < numPages;i++) {
+            var pageNum = i + 1;
+            $('#nav').append('<a href="#" rel="'+i+'">'+pageNum+'</a> ');
+        }
+        $('#zobraz tbody tr').hide();
+        $('#zobraz tbody tr').slice(0, rowsShown).show();
+        $('#nav a:first').addClass('active');
+        $('#nav a').bind('click', function(){
+            $('#nav a').removeClass('active');
+            $(this).addClass('active');
+            var currPage = $(this).attr('rel');
+            var startItem = currPage * rowsShown;
+            var endItem = startItem + rowsShown;
+            $('#zobraz tbody tr').css('opacity','0.0').hide().slice(startItem, endItem).
+                    css('display','table-row').animate({opacity:1}, 300);
+        });
+        document.getElementById('cekani').style.display = "none";
         document.getElementById('cekani').style.display = "none";
       });
     }
-    
-  
   });
-   
     document.getElementById('zobraz').addEventListener('click', function (item) {
 
       // To get tr tag 
@@ -74,4 +89,6 @@ fetch("https://data.cesko.digital/obce/1/obce.json")
       else
           row.classList.add('highlight');
   }); 
+
+  
   
